e7c9ee37a0bfd6901e05b3e065254d7c1913568f
Henry <henrykc24@gmail.com>
2022-06-09 02:10:42 +0100

    Add routes

--- index.js
+++ index.js
@@ -4,27 +4,56 @@ const cheerio = require("cheerio");
 
 const app = express();
 
-const url = "https://www.gartner.com/en/conferences/calendar";
+app.get("/api/v1/calendar", (req, res) => {
+  try {
+    const baseURL = "https://www.gartner.com";
+    const url = `${baseURL}/en/conferences/calendar`;
+    axios(url)
+      .then((result) => {
+        const html = result.data;
+        const $ = cheerio.load(html);
 
-axios(url).then((result) => {
-  const html = result.data;
-  const $ = cheerio.load(html);
+        const conferenceData = [];
 
-  const conferenceData = [];
+        const titles = $("div.conference-tile");
 
-  const titles = $("div.conference-tile");
-
-  titles.each(function (index) {
-    if ($(this).find("h6").text()) {
-      conferenceData[index] = {
-        title: $(this).find("h6").text(),
-        date: $(this).find(".conference-date").text(),
-        location: $(this).find(".conference-location").text(),
-        url: `https://www.gartner.com${$(this).find("a").attr("href")}`,
-      };
-    }
-  });
-  console.log(conferenceData);
+        tites.each(function (index) {
+          const title = $(this).find("h6").text();
+          const date = $(this).find(".conference-date").text();
+          const location = $(this).find(".conference-location").text();
+          const url = `${baseURL}${$(this).find("a").attr("href")}`;
+          if ($(this).find("h6").text()) {
+            conferenceData[index] = {
+              title,
+              date,
+              location,
+              url,
+              category: location.toLowerCase().includes("virtual")
+                ? "Virtual"
+                : "In-Person",
+            };
+          }
+        });
+        res.status(200).json({
+          status: "success",
+          dataCount: conferenceData.length,
+          data: {
+            conferenceData,
+          },
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          status: "fail",
+          message: err.message,
+        });
+      });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    });
+  }
 });
 
 const PORT = 8000;
