8980eef052307ac91002c6a66ff9ee6ac110506d
Henry <henrykc24@gmail.com>
2022-06-09 16:16:59 +0100

    Add the second route

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const conferenceRoute = require("./routes/conference");
+const eventsRoute = require("./routes/events");
 
 const app = express();
 
-app.use("/api/v1", conferenceRoute);
+app.use("/api/v1/conferences", conferenceRoute);
+app.use("/api/v1/events", eventsRoute);
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
