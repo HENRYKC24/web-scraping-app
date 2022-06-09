9b574e585bc8d9a7251b93e9f4500182af126541
Henry <henrykc24@gmail.com>
2022-06-09 12:44:47 +0100

    Modularize request handler

---
+++ controllers/conferenceController.js
@@ -0,0 +1,54 @@
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+exports.getConferences = (req, res) => {
+  try {
+    const baseURL = "https://www.gartner.com";
+    const url = `${baseURL}/en/conferences/calendar`;
+    axios(url)
+      .then((result) => {
+        const html = result.data;
+        const $ = cheerio.load(html);
+
+        const conferenceData = [];
+
+        const titles = $("div.conference-tile");
+
+        titles.each(function (index) {
+          if ($(this).find("h6").text()) {
+            const title = $(this).find("h6").text();
+            const date = $(this).find(".conference-date").text();
+            const location = $(this).find(".conference-location").text();
+            const url = `${baseURL}${$(this).find("a").attr("href")}`;
+            conferenceData[index] = {
+              title,
+              date,
+              location,
+              url,
+              category: location.toLowerCase().includes("virtual")
+                ? "Virtual"
+                : "In-Person",
+            };
+          }
+        });
+        res.status(200).json({
+          status: "success",
+          dataCount: conferenceData.length,
+          data: {
+            conferenceData,
+          },
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          status: "fail",
+          message: err.message,
+        });
+      });
+  } catch (err) {
+    res.status(500).json({
+      status: "fail",
+      message: err,
+    });
+  }
+};
