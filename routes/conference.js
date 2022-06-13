a18c9595890f467b8cf51ee40a72fcb4a1a91ff7
Henry <henrykc24@gmail.com>
2022-06-13 19:18:20 +0100

    Remove useless files

--- routes/conference.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getConferences } = require("../controllers/conferenceController");
-
-const route = express.Router();
-
-route.route("/").get(getConferences);
-
-module.exports = route;
--- routes/events.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { getEvents } = require("../controllers/eventsController");
-
-const route = express.Router();
-
-route.route("/").get(getEvents);
-
-module.exports = route;
